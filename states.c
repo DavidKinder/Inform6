60c85cb45fe4e24583a5fd1c03397165c8f7931e
Andrew Plotkin <zarf@ZarfLent.local>
2022-02-18 18:23:01 -0500

    This is tidier.

--- states.c
+++ states.c
@@ -739,8 +739,6 @@ static int parse_named_label_statements()
             return TRUE;
         }
 
-        //### statement_is_unreachable = 0; /*###?*/
-
         if (symbols[token_value].flags & UNKNOWN_SFLAG)
         {   assign_symbol(token_value, next_label, LABEL_T);
             symbols[token_value].flags |= USED_SFLAG;
@@ -2645,16 +2643,19 @@ static void parse_statement_g(int break_label, int continue_label)
 extern void parse_statement(int break_label, int continue_label)
 {
     int res;
-    int saved_unreachable = statement_is_unreachable;
-    statement_is_unreachable = execution_never_reaches_here;
+    int saved_unreachable;
 
     res = parse_named_label_statements();
-    if (res) {
-        if (!glulx_mode)
-            parse_statement_z(break_label, continue_label);
-        else
-            parse_statement_g(break_label, continue_label);
-    }
+    if (!res)
+        return;
+    
+    saved_unreachable = statement_is_unreachable;
+    statement_is_unreachable = execution_never_reaches_here;
+    
+    if (!glulx_mode)
+        parse_statement_z(break_label, continue_label);
+    else
+        parse_statement_g(break_label, continue_label);
 
     statement_is_unreachable = saved_unreachable;
 }
