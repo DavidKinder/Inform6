552cbafde8bbc682a20221f01a4d529d7c33fd11
David Kinder <davidk@davidk>
2024-02-12 19:36:40 +0000

    Release date back to 'in development'

--- header.h
+++ header.h
@@ -31,7 +31,7 @@
 /* ------------------------------------------------------------------------- */
 
 /* For releases, set to the release date in the form "1st January 2000" */
-#define RELEASE_DATE "10th February 2024"
+#define RELEASE_DATE "in development"
 #define RELEASE_NUMBER 1642
 #define GLULX_RELEASE_NUMBER 38
 #define VNUMBER RELEASE_NUMBER
