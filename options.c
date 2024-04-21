7037f417eee4a40761b3f9916eb8bd8f1e239c0b
Andrew Plotkin <zarf@ZarfLeaf.local>
2024-04-21 18:46:21 +0200

    Sorted out the options struct.

--- options.c
+++ options.c
@@ -20,28 +20,50 @@
    NUM_ATTR_BYTES is rounded up 3 mod 4.
  ### */
 
+enum optionuse {
+    OPTUSE_ALL         = 0,  /* all targets */
+    OPTUSE_ZCODE       = 1,  /* Z-code only */
+    OPTUSE_GLULX       = 2,  /* Glulx only */
+    OPTUSE_OBSOLETE_I5 = 5,  /* obsolete Inform 5 option */
+    OPTUSE_OBSOLETE_I6 = 6,  /* obsolete Inform 6 option */
+};
+
+enum optionlimit {
+    OPTLIM_ANY         = 0,  /* any non-negative value */
+    OPTLIM_TOMAX       = 1,  /* zero to N inclusive */
+    OPTLIM_MUL256      = 2,  /* any, round up to a multiple of 256 */
+    OPTLIM_3MOD4       = 3,  /* any, round up to a multiple of 3 mod 4 */
+};
+
 #define CURRENT_OPT (0)
 #define OBSOLETE_OPT_5 (5)
 #define OBSOLETE_OPT_6 (6)
 
-#define DEFAULTVAL(v) { (v), (v), }
-#define DEFAULTVALS(z, g) { (z), (g), }
+#define MAX_OPT(v) { 0, (v) }  /* zero to N inclusive */
+#define MUL256_OPT() { } /* round up to a multiple of 256 */
+
+#define DEFAULTVAL(v) { (v), (v) }
+#define DEFAULTVALS(z, g) { (z), (g) }
 
 #define DEFAULT_OPTPREC (0)
 #define HEADCOM_OPTPREC (1)
 #define CMDLINE_OPTPREC (2)
 
+typedef struct optionlimit_s {
+    enum optionlimit limittype;
+    int32 maxval;
+} optionlimitt;
+
 typedef struct platformval_s {
-    int z;
-    int g;
+    int32 z;
+    int32 g;
 } platformval;
 
 typedef struct optiont_s {
     char *name;
     char *desc;
-    int obsolete;
-    int minval;
-    int maxval;
+    enum optionuse use;
+    optionlimitt limit;
     platformval val;
     int precedence;
 } optiont;
@@ -58,8 +80,8 @@ static optiont alloptions[] = {
         "\
   HASH_TAB_SIZE is the size of the hash tables used for the heaviest \n\
   symbols banks.\n",
-        CURRENT_OPT,
-        0, -1,
+        OPTUSE_ALL,
+        { OPTLIM_ANY },
         DEFAULTVAL(512),
     },
 };
@@ -72,8 +94,7 @@ extern void prepare_options(void)
     }
 }
 
-/* Set an option to a given value. The option value is a string; most
-   options are numeric but not all.
+/* Set an option to a given value.
 
    We will enforce option limits (minima, maxima, multiple-of-N) before
    setting the value.
@@ -86,7 +107,7 @@ extern void prepare_options(void)
    or Glulx when this is called. The option structure has two values,
    perhaps differing; we will set both.
  */
-extern void set_option(char *name, char *str, int prec)
+extern void set_option(char *name, int32 val, int prec)
 {
 }
 
