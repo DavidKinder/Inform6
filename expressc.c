551bfbd162142b706c5997d479a0c74bc3a68975
Andrew Plotkin <zarf@ZarfLent.local>
2024-05-27 19:48:54 -0400

    y = x+0 cases.

--- expressc.c
+++ expressc.c
@@ -798,8 +798,18 @@ static int try_to_simplify_operand_z(int o_n,
     if (o_n == add_zc && operands_equal(&o1, &st) && o2.type == SHORT_CONSTANT_OT && o2.value == 0 && !o2.marker) {
         return TRUE;
     }
+    /* y = x + 0 ==> store */
+    if (o_n == add_zc && o2.type == SHORT_CONSTANT_OT && o2.value == 0 && !o2.marker) {
+        assemblez_store(st, o1);
+        return TRUE;
+    }
     /* x = 0 + x ==> skip */
     if (o_n == add_zc && operands_equal(&o2, &st) && o1.type == SHORT_CONSTANT_OT && o1.value == 0 && !o1.marker) {
+        return TRUE;
+    }
+    /* y = 0 + x ==> store */
+    if (o_n == add_zc && o1.type == SHORT_CONSTANT_OT && o1.value == 0 && !o1.marker) {
+        assemblez_store(st, o2);
         return TRUE;
     }
     /* x = x - 1 ==> x-- */
@@ -833,8 +843,18 @@ static int try_to_simplify_operand_g(int o_n,
     if (o_n == add_gc && operands_equal(&o1, &st) && o2.type == ZEROCONSTANT_OT && o2.value == 0 && !o2.marker) {
         return TRUE;
     }
+    /* y = x + 0 ==> store */
+    if (o_n == add_gc && o2.type == ZEROCONSTANT_OT && o2.value == 0 && !o2.marker) {
+        assembleg_store(st, o1);
+        return TRUE;
+    }
     /* x = 0 + x ==> skip */
     if (o_n == add_gc && operands_equal(&o2, &st) && o1.type == ZEROCONSTANT_OT && o1.value == 0 && !o1.marker) {
+        return TRUE;
+    }
+    /* y = 0 + x ==> store */
+    if (o_n == add_gc && o1.type == ZEROCONSTANT_OT && o1.value == 0 && !o1.marker) {
+        assembleg_store(st, o2);
         return TRUE;
     }
     /* x = x - 0 ==> skip */
