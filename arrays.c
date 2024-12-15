76309b7667cb6489d2ddfc5805707ef1707742b1
Andrew Plotkin <zarf@ZarfLent.local>
2024-12-14 20:38:33 -0500

    Comment.

--- arrays.c
+++ arrays.c
@@ -82,9 +82,10 @@ int globalv_z_sender;
 
 /* The range of global variables available to the user. These will be set
    to avoid globalv_z_temp_var1..globalv_z_sender.
-   To make things just a bit confusing, these values are in the 0..239 range.
-   The values above are shifted by 16. (Remember that variable indexes 0-15
-   are reserved for the stack pointer and locals.)
+   To make things just a bit confusing, zcode_user_global_start_no is in
+   the 0..239 range. zcode_highest_allowed_global, and the values above,
+   are shifted by 16. (Remember that variable indexes 0-15 are reserved
+   for the stack pointer and locals.)
 */
 int zcode_user_global_start_no;
 int zcode_highest_allowed_global;
