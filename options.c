6e1a0a85af1e0b0bab4c68afdc88410cbae483d0
Andrew Plotkin <zarf@ZarfLent.local>
2025-08-25 11:27:46 -0400

    Match copyright year.

--- options.c
+++ options.c
@@ -2,7 +2,7 @@
 /*   "options" : Compiler options and memory settings                        */
 /*                                                                           */
 /*   Part of Inform 6.43                                                     */
-/*   copyright (c) Graham Nelson 1993 - 2024                                 */
+/*   copyright (c) Graham Nelson 1993 - 2025                                 */
 /*                                                                           */
 /* ------------------------------------------------------------------------- */
 
